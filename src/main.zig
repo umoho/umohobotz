3d2793d9098207458891618491c935e521d227f0
umoho <my.umoho@hotmail.com>
2025-02-20 03:23:36 +0800

    feat: changed to use the Telegram Bot API URL

--- src/main.zig
+++ src/main.zig
@@ -7,14 +7,26 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    const bot_token = std.process.getEnvVarOwned(allocator, "BOT_TOKEN") catch |err| {
+        std.debug.print("tip: please set 'BOT_TOKEN'\n", .{});
+        return err;
+    };
+    defer allocator.free(bot_token);
+
+    var api_uri_prefix_concat = std.ArrayList(u8).init(allocator);
+    defer api_uri_prefix_concat.deinit();
+    try api_uri_prefix_concat.appendSlice("https://api.telegram.org/bot");
+    try api_uri_prefix_concat.appendSlice(bot_token);
+    try api_uri_prefix_concat.append('/');
+
     var bot = Bot{
         .allocator = allocator,
         .client = .{ .allocator = allocator },
-        .api_uri_prefix = "http://postman-echo.com/",
+        .api_uri_prefix = api_uri_prefix_concat.items,
     };
     defer bot.deinit();
 
-    const body = try bot.invoke("post", "", 8192);
+    const body = try bot.invoke("getMe", "", 8192);
 
     std.debug.print("body:\n{s}\n", .{body.buf});
 
