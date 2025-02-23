e5049566f900c4a4ebce639b4909de2a242b5555
umoho <my.umoho@hotmail.com>
2025-02-23 19:50:05 +0800

    feat: changed to select print format auto from object, json to plain

--- src/main.zig
+++ src/main.zig
@@ -8,14 +8,6 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    // handle the args.
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-    if (args.len != 3) {
-        std.debug.print("tip: please set '--print <plain|json|object>'\n", .{});
-        return;
-    }
-
     // read bot token.
     const bot_token = std.process.getEnvVarOwned(allocator, "BOT_TOKEN") catch |err| {
         std.debug.print("tip: please set 'BOT_TOKEN'\n", .{});
@@ -42,23 +34,8 @@ pub fn main() !void {
     const body = try bot.invoke(.getUpdates, "", 1024 * 1024);
     defer body.deinit();
 
-    if (std.mem.eql(u8, args[2], "plain")) {
-        // print as plain.
-        std.debug.print("plain:\n{s}\n", .{body.buf.items});
-    }
-
-    if (std.mem.eql(u8, args[2], "json")) {
-        // print as json.
-        const parsed_json = try body.toJson();
-        defer parsed_json.deinit();
-        std.debug.print("JSON:\n", .{});
-        parsed_json.value.dump();
-        std.debug.print("\n", .{});
-    }
-
-    if (std.mem.eql(u8, args[2], "object")) {
+    if (body.toResponseObject([]Bot.objects.Update)) |parsed_object| {
         // print as object.
-        const parsed_object = try body.toResponseObject([]Bot.objects.Update);
         defer parsed_object.deinit();
         const object = parsed_object.value;
         std.debug.print("object:\n{}\n", .{object});
@@ -93,6 +70,19 @@ pub fn main() !void {
                 }
             }
             std.debug.print("the max update ID is {}\n", .{max_update_id});
+        }
+    } else |parse_object_err| {
+        std.debug.print("failed to parse object: {}\n", .{parse_object_err});
+        if (body.toJson()) |parsed_json| {
+            // print as json.
+            defer parsed_json.deinit();
+            std.debug.print("JSON:\n", .{});
+            parsed_json.value.dump();
+            std.debug.print("\n", .{});
+        } else |parse_json_err| {
+            std.debug.print("failed to parse JSON: {}\n", .{parse_json_err});
+            // print as plain.
+            std.debug.print("plain:\n{s}\n", .{body.buf.items});
         }
     }
 }
