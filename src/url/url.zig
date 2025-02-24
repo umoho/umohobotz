31e8e2d6f6640a4b6e195d1ddc22f9e3f19bec48
umoho <my.umoho@hotmail.com>
2025-02-24 12:41:08 +0800

    feat(url): add `makeUrl` function to construct URLs with query parameters

---
+++ src/url/url.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
+
+const query_string = @import("query_string.zig");
+
+/// Make a URL from API URI prefix, method and arguments.
+///
+/// Note:
+///
+/// - Remember to free the returned URL.
+/// - If `args` is `void`, it will not add query string.
+pub fn makeUrl(
+    allocator: Allocator,
+    api_uri_prefix: []const u8,
+    method: []const u8,
+    args: anytype,
+) ![]u8 {
+    var str = std.ArrayList(u8).init(allocator);
+    defer str.deinit();
+
+    try str.appendSlice(api_uri_prefix);
+    try str.appendSlice(method);
+
+    if (@TypeOf(args) != void) {
+        const pairs = try query_string.pairsFromStruct(allocator, @TypeOf(args), args);
+        defer query_string.freePairs(allocator, @TypeOf(args), pairs);
+
+        const qs = try query_string.buildQueryString(allocator, pairs);
+        defer allocator.free(qs);
+
+        try str.appendSlice(qs);
+    }
+
+    return str.toOwnedSlice();
+}
+
+test makeUrl {
+    const allocator = std.testing.allocator;
+
+    const url = try makeUrl(allocator, "https://example.com", "/getUpdates", .{
+        .limit = 100,
+        .offset = 200,
+    });
+    defer allocator.free(url);
+
+    try std.testing.expectEqualSlices(
+        u8,
+        "https://example.com/getUpdates?limit=100&offset=200",
+        url,
+    );
+
+    const url_empty_args = try makeUrl(allocator, "https://example.com", "/getUpdates", .{});
+    defer allocator.free(url_empty_args);
+
+    try std.testing.expectEqualSlices(
+        u8,
+        "https://example.com/getUpdates",
+        url_empty_args,
+    );
+}
