3c7cffe3aa5217d325fd16e9ff282d8b48a3a5f6
umoho <my.umoho@hotmail.com>
2025-02-24 20:19:55 +0800

    refactor(root): reorganize module structure for telegram and url

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,10 @@
-const std = @import("std");
+pub const telegram = struct {
+    pub const bot = @import("telegram/bot.zig");
+    pub const client = @import("telegram/client.zig");
+    pub const objects = @import("telegram/objects.zig");
+};
 
-pub const Bot = @import("Bot.zig");
-
-pub const url = @import("url.zig");
+pub const url = struct {
+    pub const query_string = @import("url/query_string.zig");
+    pub const url = @import("url/url.zig");
+};
