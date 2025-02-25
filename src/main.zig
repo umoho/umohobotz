27cc70addd03237572e3be9335134e73ad34888c
umoho <my.umoho@hotmail.com>
2025-02-26 02:05:00 +0800

    feat(telegram): implement basic message handling and response

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 
 const Bot = @import("telegram/bot.zig").Bot;
 
+const objects = @import("telegram/objects.zig");
+const methods = @import("telegram/methods.zig");
+
 pub fn main() !void {
     // make allocator.
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -19,5 +22,50 @@ pub fn main() !void {
     var bot = try Bot.init(allocator, bot_token);
     defer bot.deinit();
 
-    try bot.getUpdatesLoop();
+    try bot.getUpdatesLoop(handleUpdate);
+}
+
+fn handleUpdate(bot: *Bot, update: objects.Update) void {
+    const message = update.message orelse {
+        std.log.warn("update message is null: {any}", .{update});
+        // tell the user we can't handle this update.
+        sendMessage(bot, update.message.chat.id, "I can't handle this update");
+        return;
+    };
+    const text = message.text orelse {
+        std.log.warn("message text is null: {any}", .{message});
+        // tell the user we can't handle this message.
+        sendMessage(bot, message.chat.id, "I can't handle this message");
+        return;
+    };
+
+    // log the message.
+    if (message.chat.username) |username| {
+        std.log.info("I got a text message from {s}: {s}\n", .{ username, text });
+    } else {
+        std.log.info("I got a text message from {d}: {s}\n", .{ message.chat.id, text });
+    }
+
+    // build a response message.
+    const response_message = std.fmt.allocPrint(
+        bot.allocator,
+        "I got your message: '{s}'",
+        .{text},
+    ) catch "I got your message";
+    defer bot.allocator.free(response_message);
+
+    // send a message back to the user.
+    sendMessage(bot, message.chat.id, response_message);
+}
+
+fn sendMessage(bot: *Bot, chat_id: i64, text: []const u8) void {
+    if (bot.invokeGet(methods.SendMessage{
+        .chat_id = chat_id,
+        .text = text,
+    })) |response| {
+        defer response.deinit();
+        std.log.info("response: {s}\n", .{response.buf});
+    } else |err| {
+        std.log.warn("failed to send message: {}\n", .{err});
+    }
 }
--- src/telegram/methods.zig
+++ src/telegram/methods.zig
@@ -1,3 +1,7 @@
+// TODO: let's generate structs by a script.
+// TODO: let buildQueryString ignore null fields.
+// TODO: find out a good way to get the method name. (and return type)
+
 pub const Name = union(enum) {
     /// See https://core.telegram.org/bots/api#getupdates.
     getUpdates,
@@ -18,27 +22,28 @@ pub const GetUpdates = struct {
     offset: ?i64 = null,
     limit: ?i64 = null,
     timeout: ?i64 = null,
-    allowed_updates: ?[][]u8 = null,
+    allowed_updates: ?[][]const u8 = null,
 };
 
 /// See https://core.telegram.org/bots/api#sendmessage.
 pub const SendMessage = struct {
-    business_connection_id: ?[]u8 = null,
-    chat_id: union { integer: i64, string: []u8 },
-    message_thread_id: ?i64 = null,
-    text: []u8,
-    parse_mode: ?[]u8 = null,
-    entities: ?[]void = null, // TODO: actual type 'MessageEntity'.
-    link_preview_options: ?void = null, // TODO: actual type 'LinkPreviewOptions'.
-    disable_notification: ?bool = null,
-    protect_connect: ?bool = null,
-    allow_paid_broadcast: ?bool = null,
-    message_effect_id: ?[]u8 = null,
-    reply_parameters: ?[]void = null, // TODO: actual type 'ReplyParameters'.
-    reply_markup: ?union {
-        inline_keyboard_markup: void,
-        reply_keyboard_markup: void,
-        reply_keyboard_remove: void,
-        force_reply: void,
-    } = null,
+    // business_connection_id: ?[]const u8 = null,
+    // chat_id: union { integer: i64, string: []const u8 },
+    chat_id: i64,
+    // message_thread_id: ?i64 = null,
+    text: []const u8,
+    // parse_mode: ?[]const u8 = null,
+    // entities: ?[]void = null, // TODO: actual type 'MessageEntity'.
+    // link_preview_options: ?void = null, // TODO: actual type 'LinkPreviewOptions'.
+    // disable_notification: ?bool = null,
+    // protect_connect: ?bool = null,
+    // allow_paid_broadcast: ?bool = null,
+    // message_effect_id: ?[]const u8 = null,
+    // reply_parameters: ?[]void = null, // TODO: actual type 'ReplyParameters'.
+    // reply_markup: ?union {
+    //     inline_keyboard_markup: void,
+    //     reply_keyboard_markup: void,
+    //     reply_keyboard_remove: void,
+    //     force_reply: void,
+    // } = null,
 };
