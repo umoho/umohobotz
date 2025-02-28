a8646be04fc05294d3cb9c4aa9ce9ec44b211ced
umoho <my.umoho@hotmail.com>
2025-03-01 01:16:44 +0800

    feat(telegram): integrate OpenRouter AI for message replies

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const Bot = @import("telegram/bot.zig").Bot;
 const objects = @import("telegram/objects.zig");
 const methods = @import("telegram/methods.zig");
 
+const OpenRouter = @import("openrouter/openrouter.zig").OpenRouter;
+
 pub fn main() !void {
     // make allocator.
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -44,20 +46,23 @@ fn handleUpdate(bot: *Bot, update: objects.Update) void {
         std.log.info("I got a text message from {d}: {s}", .{ message.chat.id, text });
     }
 
-    // build a response message.
-    const response_message = std.fmt.allocPrint(
-        bot.allocator,
-        "I got your message: '{s}'",
-        .{text},
-    ) catch "I got your message";
-    defer bot.allocator.free(response_message);
+    // get reply from openrouter.
+    const reply_text = getReply(bot.allocator, text) catch |err| {
+        std.log.warn("failed to get reply: {}", .{err});
+        sendMessage(bot, message.chat.id, "I can't reply to your message");
+        return;
+    } orelse {
+        sendMessage(bot, message.chat.id, "I can't reply to your message");
+        return;
+    };
+    defer bot.allocator.free(reply_text);
 
     // send a message back to the user.
-    sendMessage(bot, message.chat.id, response_message);
+    sendMessage(bot, message.chat.id, reply_text);
 }
 
 fn sendMessage(bot: *Bot, chat_id: i64, text: []const u8) void {
-    if (bot.client.invokeGet(methods.SendMessage{
+    if (bot.client.invokePost(methods.SendMessage{
         .chat_id = chat_id,
         .text = text,
     })) |response| {
@@ -66,4 +71,46 @@ fn sendMessage(bot: *Bot, chat_id: i64, text: []const u8) void {
     } else |err| {
         std.log.warn("failed to send message: {}", .{err});
     }
+}
+
+fn getReply(
+    allocator: std.mem.Allocator,
+    text: []const u8,
+) !?[]const u8 {
+    // read openrouter api key.
+    const openrouter_api_key = std.process.getEnvVarOwned(
+        allocator,
+        "OPENROUTER_API_KEY",
+    ) catch |err| {
+        std.debug.print("tip: please set 'OPENROUTER_API_KEY'\n", .{});
+        return err;
+    };
+    defer allocator.free(openrouter_api_key);
+
+    // initialize openrouter.
+    var openrouter = OpenRouter.init(allocator, openrouter_api_key);
+    defer openrouter.deinit();
+
+    const reply = try openrouter.chatCompletion(.{
+        .model = "deepseek/deepseek-r1:free",
+        .messages = &.{.{
+            .role = "user",
+            .content = text,
+        }},
+    });
+    defer reply.deinit();
+
+    const reply_choices = reply.value.choices orelse {
+        return null;
+    };
+    const reply_text = reply_choices[0].message.content orelse {
+        return null;
+    };
+
+    std.log.info("reply text (length: {d}): {s}", .{ reply_text.len, reply_text });
+
+    // let slice to owned.
+    const owned_text = try allocator.dupe(u8, reply_text);
+
+    return owned_text;
 }
